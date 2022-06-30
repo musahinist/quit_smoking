29a6e37cd4f1a3805abece846cc9573df8bc0f79
musahinist <sahinmusa1@gmail.com>
2022-06-30 20:51:14 +0300

    add hive local storage

--- lib/view/more/smoker_info_section.dart
+++ lib/view/more/smoker_info_section.dart
@@ -8,47 +8,47 @@ class SmokerInfoSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('Former Smoker Data'),
+      ),
       body: ListView(
         padding: const EdgeInsets.all(16),
         children: [
-          Text(
-            "Sigarayı bırakma kararınız için tebrikler!",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.headline5,
-          ),
           DateFormField(
-              label: 'Sigrayı bırakma tarihi',
+              label: 'When did you quit smoking?',
               onChanged: (value) {
                 mockUser = mockUser.copyWith(quitDate: value.toIso8601String());
               }),
+          // TextFormField(
+          //   initialValue: "20",
+          //   decoration: const InputDecoration(
+          //     label: Text('Siagra içilen yıl'),
+          //   ),
+          // ),
           TextFormField(
             initialValue: "20",
             decoration: const InputDecoration(
-              label: Text('Siagra içilen yıl'),
+              label: Text('How many cigarettes did you smoke per day?'),
             ),
           ),
           TextFormField(
             initialValue: "20",
             decoration: const InputDecoration(
-              label: Text('Günlük içilen sigara'),
-            ),
-          ),
-          TextFormField(
-            initialValue: "20",
-            decoration: const InputDecoration(
-              label: Text('1 pakette kaç sigara var'),
+              label: Text('How many cigarettes where in the pack?'),
             ),
           ),
           TextFormField(
             initialValue: "25",
             decoration: const InputDecoration(
-              label: Text('Günlük içilen sigara fiyatı'),
+              label: Text('What is the price of the pack?'),
             ),
           ),
           TextFormField(
             initialValue: "TRY",
             decoration: const InputDecoration(
-              label: Text('Para Birimi'),
+              label: Text('Currency'),
             ),
           )
         ],
